e3909161abdc2d0fb11f4b85a49be83d0cde7d45
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-03-21 14:19:48 +0100

    Implement class modal + Solve Issue #2

--- modal.js
+++ modal.js
@@ -7,31 +7,51 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const hideModalBtn = document.querySelectorAll(".close");
-const formData = document.querySelectorAll(".formData");
-
 class ModalManager{
+  
+  constructor() {
+    // DOM Elements
+    this.modalbg = document.querySelector(".bground");
+    this.modalBtn = document.querySelectorAll(".modal-btn");
+    this.hideModalBtn = document.querySelectorAll(".close");
+    this.formSubmitBtn = document.querySelector('#reserve-form input[type="submit"]');
+  }
+
   // setup Modal events
   setUpModalEvents() {
     // launch modal event
-    modalBtn.forEach((btn) => btn.addEventListener("click", this.launchModal));
+    this.modalBtn.forEach(btn => btn.addEventListener("click", () => this.launch()));
 
     // hide modal event
-    hideModalBtn.forEach((btn) => btn.addEventListener("click", this.hideModal));
+    this.hideModalBtn.forEach(btn => btn.addEventListener("click", () => this.hide()));
+
+    // handle modal form submit
+    this.formSubmitBtn.addEventListener("click", (e) => this.handleFormSubmit(e));
   }
 
   // launch modal form
-  launchModal() {
-    modalbg.style.display = "block";
+  launch() {
+    this.modalbg.style.display = "block";
   }
 
   // hide modal form
-  hideModal() {
-    modalbg.style.display = "none";
+  hide() {
+    this.modalbg.style.display = "none";
   }
+
+  // Handle modal form submit
+  handleFormSubmit(e) {
+    // Loop through form input.
+    // Each input must be valid to submit form.
+    for (let input of document.querySelectorAll("#reserve-form input")) {
+      // If input is not valid, we prevent form from being submitted
+      if (!input.checkValidity()) {
+        console.log(input)
+        console.log("is invalid");
+        e.preventDefault();
+      }
+    }
+  } 
 }
 
 const modalManager = new ModalManager();
